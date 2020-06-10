858ce51c2401a0db93a1039de370cc378bad1cc2
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-10 14:03:07 +0500

    Проблемы с 3 радиобаттоном

--- Ветвление 10.06 №1/Ветвление 10.06 №1/Form1.Designer.cs
+++ Ветвление 10.06 №1/Ветвление 10.06 №1/Form1.Designer.cs
@@ -35,42 +35,44 @@
             this.button2 = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(29, 41);
+            this.radioButton1.Location = new System.Drawing.Point(15, 38);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(142, 29);
+            this.radioButton1.Size = new System.Drawing.Size(138, 29);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "индексу = k";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(29, 76);
+            this.radioButton2.Location = new System.Drawing.Point(15, 73);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(142, 29);
+            this.radioButton2.Size = new System.Drawing.Size(117, 29);
             this.radioButton2.TabIndex = 0;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton1";
+            this.radioButton2.Text = "числу = k";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 114);
+            this.groupBox1.Size = new System.Drawing.Size(270, 193);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Определить по:";
             // 
             // button1
             // 
@@ -81,10 +83,11 @@
             this.button1.Size = new System.Drawing.Size(82, 69);
             this.button1.TabIndex = 2;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(675, 288);
+            this.button2.Location = new System.Drawing.Point(512, 240);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 52);
             this.button2.TabIndex = 3;
@@ -95,7 +98,7 @@
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(218, 12);
+            this.checkedListBox1.Location = new System.Drawing.Point(288, 12);
             this.checkedListBox1.MultiColumn = true;
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -105,16 +108,29 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(200, 245);
+            this.textBox1.Location = new System.Drawing.Point(166, 215);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 108);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(124, 29);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "цифре = k";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkedListBox1);
@@ -122,6 +138,8 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -139,6 +157,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- Ветвление 10.06 №1/Ветвление 10.06 №1/Form1.cs
+++ Ветвление 10.06 №1/Ветвление 10.06 №1/Form1.cs
@@ -28,13 +28,40 @@ namespace Ветвление_10._06__1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
-            int k = Int32.Parse(textBox1.Text);
-            for (int i = 0; i <= checkedListBox1.Items.Count - 1; i++)
+            try
             {
-                if (Convert.ToInt32(checkedListBox1.Items[i]) == k)
-                    checkedListBox1.SelectedIndex = i;
+                checkedListBox1.SelectedItems.Clear();
+                int k = Int32.Parse(textBox1.Text);
+                for (int i = 0; i <= checkedListBox1.Items.Count - 1; i++)
+                {
+                    if (radioButton2.Checked)
+                    {
+                        if (Convert.ToInt32(checkedListBox1.Items[i]) == k)
+                            checkedListBox1.SelectedIndex = i;
+                    }
+                    if (radioButton1.Checked)
+                    {
+                        if (i == k)
+                            checkedListBox1.SelectedIndex = i;
+                    }
+                    if (radioButton3.Checked)
+                    {
+                        string s = checkedListBox1.Items[i].ToString();
+                        string kk = k.ToString();
+                        for (int l = 0; l < s.Length; l++)
+                        {
+                            if (s[l] == kk[l])
+                                checkedListBox1.SelectedIndex = i;
+                        }
+                    }
+                }
             }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Даны целое число k (1<k<252) и последовательность цифр 505152...9899100101...149150, в которой выписаны подряд все натуральные числа от 50 до 250. Определить k-ю цифру.", "Задание", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
